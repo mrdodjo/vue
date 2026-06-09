cfc47561a5c74f7dc754a93bbb96c48122f43fa1
mrdodjo <baardagaam27@gmail.com>
2026-06-09 16:40:35 +0200

    Initial commit

---
+++ opdrachtnaam/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {ref, reactive} from 'vue';
+
+const firstName = ref('');
+const clicks = ref(0);
+const incrementClicks = () => {
+    clicks.value++;
+};
+const person = reactive({firstName: '', country: ''});
+const changeCountry = newCountry => {
+    person.country = newCountry;
+};
+</script>
+
+<template>
+    <div>
+        <button @click="incrementClicks">Klik mij!</button>
+        <p>clicks: {{ clicks }}</p>
+    </div>
+    <div>
+        <h2>Hallo {{ firstName }}</h2>
+        <input v-model="firstName" placeholder="Typ je naam in" />
+    </div>
+    <div>
+        <button @click="changeCountry('Nederland')">Nederland</button>
+        <button @click="changeCountry('België')">België</button>
+        <p>country: {{ person.country }}</p>
+    </div>
+</template>
---
+++ v-forDirective/v-for directive/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, reactive } from "vue";
+
+const tasks = ref([
+  { name: "Boodschappen doen", completed: false },
+  { name: "Afwassen", completed: true },
+  { name: "Hond uitlaten", completed: false },
+]);
+
+const completion = (task) => {
+  tasks.completed = !task.completed;
+};
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        <button @click="completion(task)">Toggle</button>
+        {{ task.name }}
+        {{ task.completed ? "(voltooid)" : "(niet voltooid)" }}
+      </li>
+    </ul>
+  </div>
+</template>
