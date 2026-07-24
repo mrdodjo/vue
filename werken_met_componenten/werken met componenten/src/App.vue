2aaf2aecc380be80241fe1c0cbda55678617f35e
mrdodjo <baardagaam27@gmail.com>
2026-07-24 15:38:41 +0200

    components
    yet to be figured out

--- werken_met_componenten/werken met componenten/src/App.vue
+++ werken_met_componenten/werken met componenten/src/App.vue
@@ -1,7 +1,9 @@
-<script>
-
-</script>
-
 <template>
-  <HelloWorld />
+  <ChildComponent :title="title" />
 </template>
+
+<script setup>
+import ChildComponent from './ChildComponent.vue';
+
+const title = "Welkom in Vue 3!"
+</script>
---
+++ werken_met_componenten/werken met componenten/src/components/ChildComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+});
+</script>
