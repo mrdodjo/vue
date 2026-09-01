c81c3f883b13cd902f87a57fa6a03acd55611d5e
mrdodjo <baardagaam27@gmail.com>
2026-09-01 15:55:53 +0200

    1-9-2926 progress

--- vue yahtzee/src/components/ScoreTable.vue
+++ vue yahtzee/src/components/ScoreTable.vue
@@ -46,32 +46,37 @@
         <tr>
             <td>three of a kind</td>
             <td>totaal 5 stenen</td>
-            <td class="lowerTotal" id="threeOfAKind">0</td>
+            <td v-if="threeOfAKind">{{ diceSum }}</td>
+            <td v-else>0</td>
         </tr>
         <tr>
             <td>carré</td>
             <td>totaal 5 stenen</td>
-            <td class="lowerTotal" id="carré">0</td>
+            <td v-if="carré">{{ diceSum }}</td>
+            <td v-else>0</td>
         </tr>
         <tr>
             <td>full house</td>
             <td>25 punten</td>
-            <td class="lowerTotal" id="fullHouse">0</td>
+            <td v-if="fullHouse">25</td>
+            <td v-else>0</td>
         </tr>
         <tr>
             <td>kleine straat</td>
             <td>30 punten</td>
-            <td class="lowerTotal" id="smallStraight">0</td>
+            <td v-if="smallStraight">30</td>
+            <td v-else>0</td>
         </tr>
         <tr>
             <td>grote straat</td>
             <td>40 punten</td>
-            <td class="lowerTotal" id="largeStraight">0</td>
+            <td v-if="largeStraight">40</td>
+            <td v-else>0</td>
         </tr>
         <tr>
             <td>Yahtzee</td>
             <td>50 punten</td>
-            <td v-if="diceCounter.value >= 2">50</td>
+            <td v-if="yahtzee">50</td>
             <td v-else>0</td>
         </tr>
         <tr>
@@ -118,15 +123,49 @@ const diceSum = computed(() => {
     return Sum;
 });
 
-const Yahtzee = computed(() => {
-    const yahtzeeScore = diceCounter.value.find(number => number >= 2);
-    console.log(yahtzeeScore);
+const yahtzee = computed(() => {
+    const yahtzeeScore = Object.values(diceCounter.value).some(count => count >= 5);
     return yahtzeeScore;
+});
+
+const carré = computed(() => {
+    const carréScore = Object.values(diceCounter.value).some(count => count >= 4);
+    return carréScore;
+});
+
+const threeOfAKind = computed(() => {
+    const threeScore = Object.values(diceCounter.value).some(count => count >= 3);
+    return threeScore;
+});
+
+const fullHouse = computed(() => {
+    const fullHouseScore = Object.values(diceCounter.value).includes(3) && Object.values(diceCounter.value).includes(2);
+    return fullHouseScore;
+});
+
+const largeStraight = computed(() => {
+    const equalOrSmallerThanOne = element => {
+        return element <= 1;
+    };
+    const largeStraightScore =
+        (Object.values(diceCounter.value)[0] == 0 || Object.values(diceCounter.value)[5] == 0) &&
+        Object.values(diceCounter.value).every(equalOrSmallerThanOne);
+    return largeStraightScore;
+});
+
+const smallStraight = computed(() => {
+    const smallStraightScore =
+        Object.values(diceCounter.value)[2] >= 1 &&
+        Object.values(diceCounter.value)[3] >= 1 &&
+        ((Object.values(diceCounter.value)[0] >= 1 && Object.values(diceCounter.value)[1] >= 1) ||
+            (Object.values(diceCounter.value)[1] >= 1 && Object.values(diceCounter.value)[4] >= 1) ||
+            (Object.values(diceCounter.value)[4] >= 1 && Object.values(diceCounter.value)[5] >= 1));
+    return smallStraightScore;
 });
 
 const test = () => {
     console.log(diceArray.value);
     console.log(diceCounter.value);
-    console.log(Yahtzee.value);
+    console.log(yahtzee.value);
 };
 </script>
