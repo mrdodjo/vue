34ba38f70fde50a79c6f5dac5be89946bed451f8
mrdodjo <baardagaam27@gmail.com>
2026-09-11 15:52:10 +0200

    update 11-9

--- vue boodschappenlijst/src/App.vue
+++ vue boodschappenlijst/src/App.vue
@@ -1,3 +1,19 @@
-<script setup></script>
+<template>
+    <h1>boodschappenlijst</h1>
+    <div>
+        <table>
+            <tr>
+                <td>
+                    <li v-for="product in products">{{ product }}</li>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div></div>
+</template>
 
-<template></template>
+<script setup>
+import {ref, reactive, computed} from 'vue';
+
+const products = ref(['koekjes', 'noten', 'broccoli', 'rijst']);
+</script>
