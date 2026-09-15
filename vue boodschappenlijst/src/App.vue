000cdf8463cceb0d23172ff479383e4fad5598fe
mrdodjo <baardagaam27@gmail.com>
2026-09-15 16:04:02 +0200

    15-9-2026

--- vue boodschappenlijst/src/App.vue
+++ vue boodschappenlijst/src/App.vue
@@ -2,18 +2,67 @@
     <h1>boodschappenlijst</h1>
     <div>
         <table>
+            <thead>
+                <th>Product</th>
+                <th>Prijs</th>
+                <th>Hoeveelheid</th>
+                <th>Subtotaal</th>
+            </thead>
             <tr>
                 <td>
-                    <li v-for="product in products">{{ product }}</li>
+                    <ul v-for="product in products">
+                        {{
+                            product.product
+                        }}
+                    </ul>
+                </td>
+                <td>
+                    <ul v-for="price in products">
+                        {{
+                            price.price
+                        }}
+                    </ul>
+                </td>
+                <td>
+                    <ul v-for="p in products">
+                        <input v-model.number="p.amount" type="number" class="input" min="0" />
+                    </ul>
+                </td>
+                <td>
+                    <ul v-for="product in products">
+                        {{
+                            (product.price * product.amount).toFixed(2)
+                        }}
+                    </ul>
                 </td>
             </tr>
+            <tr>totaal</tr>
         </table>
+        <p>{{ products }}</p>
     </div>
-    <div></div>
+    <div><button @click="log">log</button></div>
 </template>
 
 <script setup>
 import {ref, reactive, computed} from 'vue';
 
-const products = ref(['koekjes', 'noten', 'broccoli', 'rijst']);
+const products = ref([
+    {product: 'koekjes', price: 1.2, amount: 0},
+    {product: 'noten', price: 2.99, amount: 0},
+    {product: 'broccoli', price: 0.99, amount: 0},
+    {product: 'rijst', price: 1.0, amount: 0},
+]);
+
+const total = computed(() => {
+    for (i = 0; i < products.value.length; i++) {
+        products.value[i].price * products.value[i].amount;
+    } //.reduce((p, a) => p + a, 0);
+});
+
+const log = () => {
+    console.log(products.value[0].price);
+    console.log(products.value[0].subtotal);
+    console.log(Subtotal);
+    console.log(products.value.length);
+};
 </script>
